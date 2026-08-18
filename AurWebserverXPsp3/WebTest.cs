9b33092aeb34def3460729f888bea64bdf627bfb
FriskOrTheLucas <lucascab1999@gmail.com>
2026-08-17 20:24:16 -0700

    RAAAh

---
+++ AurWebserverXPsp3/WebTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+namespace AurWebserverXPsp3
+{
+    public class WebTest
+    {
+        public void WebSlfTest()
+        {
+            Debug.WriteLine("Hello!");
+        }
+
+    }
+
+}
