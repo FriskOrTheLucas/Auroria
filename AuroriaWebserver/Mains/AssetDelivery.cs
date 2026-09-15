9e184160cc7ad7f4bb19dcf83c31517bbf514c23
Frisk/Lucas <onebittwofd@gmail.com>
2026-09-14 20:04:15 -0700

    ASSET DELIVERY FINALLY WORKS

--- AuroriaWebserver/Mains/AssetDelivery.cs
+++ AuroriaWebserver/Mains/AssetDelivery.cs
@@ -30,36 +30,57 @@ namespace AuroriaWebserver.Mains
 
             if (cookiecheck == null)
             {
+                Console.WriteLine("No .ROBLOSECURITY token found in cookie.txt");
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
                 response.ContentType = "application/json";
 
                 string jsonResponse = "{\"message\": \"IDIOT!!! NO ROBLOSECURITY TOKEN MY SONION!!\"}";
+                Console.WriteLine("Sent error code and message.");
                 byte[] buffer = Encoding.UTF8.GetBytes(jsonResponse);
                 response.ContentLength64 = buffer.Length;
                 response.OutputStream.Write(buffer, 0, buffer.Length);
                 response.OutputStream.Close();
+                Console.WriteLine("Output has been closed.");
             }
 
             else
             {
-                string url = AsstUrl + id;
+                try
+                {
+                    // i gotta add notes to this code so then JayJay can understand
+                    // + incase someone needs to read it and understand it
+                    // also then i can laugh at funny comments later
 
-                HttpWebRequest assetReq = (HttpWebRequest)WebRequest.Create(url);
-                assetReq.Headers.Add(HttpRequestHeader.Cookie, ".ROBLOSECURITY=" + cookiecheck);
+                    string url = AsstUrl + id + "&version=" + version; //gets the asset url and id (version too for older stuff i think)
+                    Console.WriteLine("Requesting asset from: " + url); 
 
-                assetReq.AllowAutoRedirect = true;
-                assetReq.UserAgent = "Roblox/WinInet";
-                HttpWebResponse assetResponse = (HttpWebResponse)assetReq.GetResponse();
+                    HttpWebRequest assetReq = (HttpWebRequest)WebRequest.Create(url);
+                    assetReq.Headers.Add(HttpRequestHeader.Cookie, ".ROBLOSECURITY=" + cookiecheck); // requests the asset url along with the cookie
+                    Console.WriteLine("Requested URL with Cookie.");
 
-                StreamReader reader = new StreamReader(assetResponse.GetResponseStream());
-                string result = reader.ReadToEnd();
-                result = result.Replace("roblox.com", "rbxweb.lol");
+                    assetReq.AllowAutoRedirect = true; //dont know what this was i just got told to use it
+                    assetReq.Method = "GET"; //same here
+                    assetReq.UserAgent = "Roblox/WinInet"; //I learned that if youre requesting it from the actual roblox assetdelivery url then this is needed
+                    HttpWebResponse assetResponse = (HttpWebResponse)assetReq.GetResponse(); // Gets the response from the asset delivery url
+                    Console.WriteLine("Received response from asset request.");
 
-                byte[] buffer = Encoding.UTF8.GetBytes(result);
+                    Stream ResponseStream = assetResponse.GetResponseStream(); 
+                    ResponseStream.CopyTo(response.OutputStream); // Takes the respone from the asset url and copies it to the output
+                    response.OutputStream.Close();
+                    Console.WriteLine("Asset delivery Finished!"); // yaaay
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error occurred while handling asset delivery: " + ex.Message); // frick.
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; // returns 404
 
-                response.ContentLength64 = buffer.Length;
-                response.OutputStream.Write(buffer, 0, buffer.Length);
-                response.OutputStream.Close();
+                    string jsonResponse = "{\"message\": \"An error has occurred and Auroria cannot deliver the asset. We are sorry.\"}"; // i cry
+                    Console.WriteLine("Sent error code and message.");
+                    byte[] buffer = Encoding.UTF8.GetBytes(jsonResponse);
+                    response.ContentLength64 = buffer.Length;
+                    response.OutputStream.Write(buffer, 0, buffer.Length);
+                    response.OutputStream.Close();
+                }
             }
         }
     }
